673c22f0e6f3efeece2f3a32d563b0758cd2d7e2
chaityanya <chaityanyat@gmail.com>
2017-03-11 17:45:06 +0530

    first commit

---
+++ src/main/java/com/home/project/PracticeSession/App.java
@@ -0,0 +1,13 @@
+package com.home.project.PracticeSession;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/home/project/PracticeSession/AppTest.java
@@ -0,0 +1,17 @@
+package com.home.project.PracticeSession;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	
+    public void testfApp()
+    {
+        System.out.println("!---------Run is completed----------!");
+    }
+}
